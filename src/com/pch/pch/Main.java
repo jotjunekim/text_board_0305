a8c47eb3264599ef22b30688b0947796804130f7
jotjunekim <park82602@gmail.com>
2023-03-05 23:21:54 +0900

    시작 메시지 알림

---
+++ src/com/pch/pch/Main.java
@@ -0,0 +1,10 @@
+package com.pch;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("== 게시판 v 0.1 ==");
+    System.out.println("== 프로그램 시작 ==");
+    System.out.println("명령)");
+    System.out.println("== 프로그램 종료 ==");
+  }
+}
